b0f78e4c1f4f1272e05da9496a4412ae4cc7199e
Hydén Roland RGS VS SYSTEMUTV IT <rhn014@lthalland.se>
2019-01-10 15:34:31 +0100

    Intial commit

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# region-halland-acf-add-theme-subpage
-Plugin som skapar en tema-sida i WP-admin
+# Skapa en tema-sida i WP-admin
+
+## Hur man använder Region Hallands plugin "region-halland-acf-add-theme-subpage"
+
+Nedan följer instruktioner hur du kan använda pluginet "region-halland-acf-add-theme-subpage".
+
+
+## Användningsområde
+
+Denna plugin skapar en sida i WP-admin som heter "Temainställningar".
+Denna sida blir en subsida till "themes.php".
+Man kan nå sidan via "Utseende/Temainställningar".
+Till denna sida kan man sedan knyta olika formulär, ex texter för cookie-notis
+
+
+## Installation och aktivering
+
+```sh
+A) Hämta pluginen via Git eller läs in det med Composer
+B) Installera Region Hallands plugin i Wordpress plugin folder
+C) Aktivera pluginet inifrån Wordpress admin
+```
+
+
+## Hämta hem pluginet via Git
+
+```sh
+git clone https://github.com/RegionHalland/region-halland-acf-add-theme-subpage.git
+```
+
+
+## Läs in pluginen via composer
+
+Dessa två delar behöver du lägga in i din composer-fil
+
+Repositories = var pluginen är lagrad, i detta fall på github
+
+```sh
+"repositories": [
+  {
+    "type": "vcs",
+    "url": "https://github.com/RegionHalland/region-halland-acf-add-theme-subpage.git"
+  },
+],
+```
+Require = anger vilken version av pluginen du vill använda, i detta fall version 1.0.0
+
+OBS! Justera så att du hämtar aktuell version.
+
+```sh
+"require": {
+  "regionhalland/region-halland-acf-add-theme-subpage": "1.0.0"
+},
+```
+
+
+## Versionhistorik
+
+### 1.0.0
+- Första version
---
+++ region_halland_acf_add_theme_subpage.php
@@ -0,0 +1,68 @@
+<?php
+
+	/**
+	 * @package Region Halland ACF Add Theme Subpage
+	 */
+	/*
+	Plugin Name: Region Halland ACF Add Theme Subpage
+	Description: Skapa en subsida till theme.php för extrafält
+	Version: 1.0.0
+	Author: Roland Hydén
+	License: MIT
+	Text Domain: regionhalland
+	*/
+
+	// Anropa funktion om ACF är installerad & aktiverad
+	add_action('acf/init', 'region_halland_acf_add_theme_subpage');
+
+	// Lägg till en tema-sida
+	function region_halland_acf_add_theme_subpage() {
+
+		// Om funktionen existerar
+		if (function_exists('acf_add_options_sub_page')) {
+			
+			// Titel högst upp på tema-sidan
+			$strPageTitle		= 'Temainställningar';
+
+			// Titel i WP-admin sidebar
+			$strMenuTitle		= 'Temainställningar';
+
+			// Vad denna sidan ska vara knuten till, i detta fall "themes.php"
+			$strParentSlug		= 'themes.php';
+
+			// Vem som kan se sidan, i detta fall bara administratörer
+			$strCapabillity		= 'administrator';
+
+			// Om redirect = "false" kommer denna subsida att visas parallellt med förälder-sidan
+			$strRedirect		= false;
+
+			// Skapa subsida
+			acf_add_options_sub_page(array(				
+				'page_title'    => $strPageTitle,
+				'menu_title'    => $strMenuTitle,
+				'parent_slug'   => $strParentSlug,
+				'capability'    => $strCapabillity,
+				'redirect'      => $strRedirect			
+			));
+
+		}
+	
+	}
+
+	// Metod som anropas när pluginen aktiveras
+	function region_halland_acf_add_theme_subpage_activate() {
+		// Ingenting just nu...
+	}
+
+	// Metod som anropas när pluginen av-aktiveras
+	function region_halland_acf_add_theme_subpage_deactivate() {
+		// Ingenting just nu...
+	}
+
+	// Aktivera pluginen och anropa metod
+	register_activation_hook( __FILE__, 'region_halland_acf_add_theme_subpage_activate');
+
+	// Av-aktivera pluginen och anropa metod
+	register_deactivation_hook( __FILE__, 'region_halland_acf_add_theme_subpage_deactivate');
+
+?>
